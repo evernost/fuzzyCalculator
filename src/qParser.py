8d3675b59701dc28ecf36d802d773558ab6c37e4
Quentin Biache <nitrogenium@outlook.fr>
2024-06-24 00:08:12 +0200

    Added descriptions Old code cleanup
    Dev on-going.

--- src/qParser.py
+++ src/qParser.py
@@ -47,7 +47,7 @@
 # Function cannot be called with empty arguments.
 #
 # [R5] IMPLICIT MULTIPLICATIONS RULES
-# The following priorities have been defined based on the most "natural"
+# The following priorities have been defined based on what feels the most "natural"
 # interpretation. In case of ambiguity, a warning is raised.
 # [R5.1]  "1X"     -> 1*var("X")                 See R2.
 # [R5.2]  "1.0X"   -> 1.0*var("X")               See R2.
@@ -57,7 +57,8 @@
 # [R5.6]  "X3Y"    -> var("X3")*var("Y")         Not natural, raises a warning.
 # [R5.7]  "pi4.0X" -> const("pi")*4.0*var("X")   Acceptable.
 # [R5.8]  "pi4X"   -> const("pi")*4*var("X")     Acceptable, but raises a warning.
-# [R5.9]  "pixel"  -> var("pixel")               The variable as a whole is more likely than a const product
+# [R5.9]  "pixel"  -> var("pixel")               The variable as a whole is more likely than a const/variables product.
+#                                                Otherwise, why not even "p*i*x*e*l"?
 # [R5.10] "pi_5"   -> var("pi_5")                Underscore serves as disembiguation/indexing and overrides the constant
 # [R5.11] "pipi"   -> var("pipi")                If "pi*pi" was meant, maybe the user should make an effort here
 # [R5.12] "inf"    -> const("inf")               Parser tries to see as a whole in priority (so not "i*nf", "i" being a constant too)
@@ -125,9 +126,9 @@ class Token :
   #  - underscores in the constants are not allowed
   CONSTANTS = [
     {"name": "pi" , "value": 3.14159265358979},
-    {"name": "inf", "value": 0.0},
+    {"name": "i",   "value": 0.0},
     {"name": "eps", "value": 0.0},
-    {"name": "i"  , "value": 0.0},
+    {"name": "inf", "value": 0.0}
   ]
   
   FUNCTIONS = [
@@ -365,15 +366,13 @@ class QParser :
 
 
 
-
-
   # -----------------------------------------------------------------------------
-  # consumeConst(<string>)
+  # METHOD: consumeConst(<string>)
   # -----------------------------------------------------------------------------
   def consumeConst(self, input = "") :
     """
     Description:
-    Consume the leading constant name in a string.
+    Consume the leading constant in a string.
 
     If <input> is a string starting with the name of a constant, the tuple (c, rem) is 
     returned, where:
@@ -384,8 +383,9 @@ class QParser :
 
     If <input> does not start with a known constant or the constant is embedded 
     in a larger name, the tuple ("", input) is returned.
+    Refer to rules [5.X] for more details about the parsing strategy.
  
-    The list of available constants is fetched from Token.CONSTANTS.
+    The list of available constants is fetched from 'Token.CONSTANTS'.
 
     Examples:
     (See unit tests)
@@ -425,8 +425,7 @@ class QParser :
 
           else :
             return (head, tail)
-          
-          
+
     # Case 3: never matched
     return ("", inputStr)
 
@@ -502,7 +501,7 @@ class QParser :
   def consumeFunc(self, input = "") :
     """
     Description:
-    Consume the leading function name in a string.
+    Consume the leading function in a string.
 
     If <input> is a string starting with a function, the tuple (f, rem) is 
     returned, where:
@@ -514,7 +513,7 @@ class QParser :
     If <input> does not start with a known function, the tuple ("", input) is 
     returned.
  
-    The list of available functions is fetched from Token.FUNCTIONS.
+    The list of available functions is fetched from 'Token.FUNCTIONS'.
 
     Notes:
     - The function name must be immediatly followed by an opening parenthesis "(".
@@ -554,90 +553,175 @@ class QParser :
 
 
 
-# -----------------------------------------------------------------------------
-# consumeVar(<string>)
-# -----------------------------------------------------------------------------
-def consumeVar(inputStr) :
-  """
-  Description:
-  If <inputStr> is a string starting with a plausible variable name, the 
-  function returns a tuple with 2 elements:
-  - the extracted variable
-  - the rest of the string
-  The function will try to consume the input string as much as possible as long
-  as it is a valid variable name (see restrictions on variables name)
-  If <inputStr> does not start with a valid variable-looking string, a tuple with 
-  an empty string will be returned (this can also be a test to know whether 
-  <inputStr> starts with a number or not)
-  If the parsing fails at some point (see examples), the function also returns
-  an empty string.
-  <parseVar> will try to match a legal variable with existing ones. If it does 
-  not find anything, it will add it to the list of variables.
-  Illegal variable names make <parseVar> fail and nothing is added to the list
-  of variables.
-  
-  Notes: 
-  None.
+  # ---------------------------------------------------------------------------
+  # METHOD: consumeVar(<string>)
+  # ---------------------------------------------------------------------------
+  def consumeVar(self, input = "") :
+    """
+    Description:
+    Consume the leading variable in a string.
 
-  Known limitations:
-  None.
+    If <input> is a string starting with a variable, the tuple (v, rem) is 
+    returned, where:
+      - <v> is the matching variable name
+      - <rem> is the rest of the string.
+    
+    so that input = v || rem
 
-  Examples:
-  > parseVar("a*b") = ("a", "*b")
-  > parseVar("a42") = ("a42", "")
-  > parseVar(" myVar") = ("", "myVar")
-  > parseVar("zinzin_64_32-12") = ("zinzin_64_32", "-12")
-  (See unit tests for more examples)
-  """
+    Several rules apply to the parsing strategy. 
+    In short, the function tries to match it with known or declared variables.
+    If it fails, it adds it to the variable list.
 
-  # Input guard
-  assert isinstance(inputStr, str), "<parseVar> expects a string as an input."
+    Refer to rules [5.X] for more details about the parsing strategy. 
 
-  # Test the first character.
-  # If it is not a letter, don't bother (L4)
-  if not(inputStr[0].isalpha()) :
+    Examples:
+    (See unit tests)
+    """
+
+    if (len(input) > 0) :
+      inputStr = input
+    else :
+      inputStr = self.input
+
+    # Input guard
+    assert isinstance(inputStr, str), "<consumeVar> expects a string as an input."
+
+    # Test the first character.
+    # If it is not a letter, don't bother (R2)
+    if not(isAlpha(inputStr)) :
+      return ("", inputStr)
+
+    for n in range(1, len(inputStr)+1) :
+      (head, tail) = split(inputStr, n)
+      if (head in self.variables) :
+        
+        # Case 1: the whole string matches with a known variable
+        if (n == len(inputStr)) :
+          return (head, tail)
+        
+        # Case 2: there is a match, but something comes after
+        else :
+          
+          # Detect anything that could stop treating it as a variable
+          # - a number with decimal point: rule [R5.5]
+          # - a decimal point
+          # ?
+          print("TODO")
+          
+
+    # Case 3: never matched
     return ("", inputStr)
 
-  print("TODO!")
-
-  # # Start with the first character, then gradually take more characters from 
-  # # the input string, until eventually taking all of it.
-  # # The longest string that passed the <isVariable> test becomes the candidate
-  # # (if the test passed at all)
-  # # TODO: optimisation possible = s'arrêter dès que isFunction devient faux (aucune chance que ça redevienne vrai)
-  # lastValid = 0
-  # for n in range(1,len(inputStr)+1) :
-  #   (head,_) = split(inputStr,n)
-  #   if isVariable(head) :
-  #     lastValid = n
-  
-  # (tmpVar, tail) = split(inputStr,lastValid)
-  # if not(tmpVar in variables) :
-  #   variables.append(tmpVar)
-
-  # if tmpVar in functions :
-  #   print("ERROR: bad function call.")
-
-  # return (tmpVar, tail)
 
 
 
 
 
 
+  # ---------------------------------------------------------------------------
+  # METHOD: consumeInfix(<string>)
+  # ---------------------------------------------------------------------------
+  def consumeInfix(self, input = "") :
+    """
+    Description:
+    Consume the leading infix operator in a string.
+
+    If <input> is a string starting with an infix operator, the tuple (op, rem) is 
+    returned, where:
+      - <op> is the matching infix operator name
+      - <rem> is the rest of the string.
+    
+    so that input = op || rem
+
+    If <input> does not start with a known infix operator, the tuple ("", input) is returned.
+ 
+    The list of available infix operators is fetched from Token.INFIX_OPS.
+
+    Examples:
+    (See unit tests)
+    """
+
+    if (len(input) > 0) :
+      inputStr = input
+    else :
+      inputStr = self.input
+
+    # Input guard
+    assert isinstance(inputStr, str), "<consumeConst> expects a string as an input."
+
+    constList = [c["name"] for c in Token.CONSTANTS]
+
+    for n in range(1, len(inputStr)+1) :
+      (head, tail) = split(inputStr, n)
+      if (head in constList) :
+        
+        # Case 1: the whole string matches with a known constant
+        if (n == len(inputStr)) :
+          return (head, tail)
+        
+        # Case 2: there is a match, but something comes after
+        else :
+          nextChar = tail[0]
+          
+          # See [R5.10]: underscore forbids to treat as a constant
+          if (nextChar == "_") :
+            return ("", inputStr)
+          
+          # From that point: the only way to match is to have a bigger
+          # constant name, whose beginning matched with a known constant (see [R5.12])
+          # Can't conclude.
+          elif isAlpha(nextChar) :  
+            pass
+
+          else :
+            return (head, tail)
+          
+          
+    # Case 3: never matched
+    return ("", inputStr)
+
+    #   Notes:
+    #   TODO: comment distinguer le "-" pour l'opposé et le "-" opérateur infixe ?
+    #   Mais au pire, est-ce vraiment grave de voir un infixe alors que c'est une 
+    #   négation ?
+    #   C'est juste que le premier argument de l'infixe peut être omis dans certains cas.
+
+
+    #   Known limitations:
+    #   None.
+
+    #   Examples:
+    #   > infixOps  = ["+", "-", "*", "/", "^", "//", ":="]
+    #   > parseInfix("+3x") = ("+", "3x")
+    #   > parseInfix("//(12cos(pi))") = ("//", "(12cos(pi))")
+    #   > parseInfix("-3.14*cos(12)") = ("", "-3.14*cos(12)") # Piège : ça ce n'est pas un infixe ou si ?
+    #   (See unit tests for more examples)
+    #   """
+      
+    #   # Input guard
+    #   assert isinstance(inputStr, str), "<parseInfix> expects a string as an input."
+
+    #   # Start with the first character, then gradually take more characters from 
+    #   # the input string, until eventually taking all of it.
+    #   # The longest string that passed the <isFunction> test becomes the candidate
+    #   # (if the test passed at all)
+    #   # TODO: optimisation possible = s'arrêter dès que isFunction devient faux (aucune chance que ça redevienne vrai)
+    #   lastValid = 0
+    #   for n in range(1,len(inputStr)+1) :
+    #     (head,_) = split(inputStr,n)
+    #     if (head in infixOps) :
+    #       lastValid = n
+      
+    #   return split(inputStr,lastValid)
 
 
 
 
 
 
-
-
-
-
-  # -----------------------------------------------------------------------------
+  # ---------------------------------------------------------------------------
   # METHOD: tokenize(<string>)
-  # -----------------------------------------------------------------------------
+  # ---------------------------------------------------------------------------
   def tokenize(self, input = "") :
     """
     Description:
@@ -646,23 +730,6 @@ def consumeVar(inputStr) :
     The input characters are read, grouped and classified as abstracted types
     (Token objects) while preserving their information.
 
-    
- 
-    The list of available functions is fetched from Token.FUNCTIONS.
-
-    Notes:
-    - The following tests must have passed before calling this function: 
-      - TBD
-      - TBD
-    - Opening parenthesis is omitted because later in the parsing engine, a function 
-    or a single "(" triggers the same processing. 
-    So for the rest of the processing, the "(" following the function is redundant.
-    
-    Known limitations:
-    None.
-
-    Examples:
-    > parseFunc("sina") = ("", "sina")
     
     """
 
@@ -679,7 +746,7 @@ def consumeVar(inputStr) :
       # Try all possibilities
       # Only 1 test must pass!
       (number, number_t) = self.consumeNumber(inputStr)
-      (constant, constant_t) = self.consumeConstant(inputStr)
+      (constant, constant_t) = self.consumeConst(inputStr)
       (function, function_t) = self.consumeFunction(inputStr)
       (variable, variable_t) = self.consumeVariable(inputStr)
 
@@ -835,101 +902,6 @@ def isNumber(inputStr) :
 
 
 
-# # -----------------------------------------------------------------------------
-# # isVariable(<string>)
-# # -----------------------------------------------------------------------------
-# def isVariable(inputStr) :
-#   """
-#   Description:
-#   Detect if a given string is a legal variable name.
-  
-#   Known limitations: 
-#   None.
-  
-#   Examples:
-#   > isVariable("Martine") = True
-#   > isVariable("x2") = True
-#   > isVariable("_karen") = False
-#   (See unit tests for more examples)
-#   """
-  
-#   # Input guard
-#   assert isinstance(inputStr, str), "<isVariable> expects a string as an input."
-
-#   # A variable must start with a letter (L4)
-#   if not(inputStr[0].isalpha()) :
-#     return False
-
-#   for char in inputStr :
-#     if not((char.isalpha()) or (char.isnumeric()) or (char == "_")) :
-#       return False
-
-#   return True
-
-
-
-
-
-
-
-
-
-# # -----------------------------------------------------------------------------
-# # parseInfix(<string>)
-# # -----------------------------------------------------------------------------
-# def parseInfix(inputStr) :
-#   """
-#   Description:
-#   If <inputStr> is a string starting with an infix operator, the function returns 
-#   a tuple with 2 elements:
-#   - the extracted infix operator
-#   - the rest of the string
-#   If <inputStr> does not start with a good candidate infix operator, the first 
-#   element of the tuple is an empty string; this can be a test to know whether 
-#   <inputStr> starts with an infix operator or not.
-#   If the parsing fails at some point (see examples), the first 
-#   element of the tuple is also an empty string.
-
-#   Notes:
-#   TODO: comment distinguer le "-" pour l'opposé et le "-" opérateur infixe ?
-#   Mais au pire, est-ce vraiment grave de voir un infixe alors que c'est une 
-#   négation ?
-#   C'est juste que le premier argument de l'infixe peut être omis dans certains cas.
-
-
-#   Known limitations:
-#   None.
-
-#   Examples:
-#   > infixOps  = ["+", "-", "*", "/", "^", "//", ":="]
-#   > parseInfix("+3x") = ("+", "3x")
-#   > parseInfix("//(12cos(pi))") = ("//", "(12cos(pi))")
-#   > parseInfix("-3.14*cos(12)") = ("", "-3.14*cos(12)") # Piège : ça ce n'est pas un infixe ou si ?
-#   (See unit tests for more examples)
-#   """
-  
-#   # Input guard
-#   assert isinstance(inputStr, str), "<parseInfix> expects a string as an input."
-
-#   # Start with the first character, then gradually take more characters from 
-#   # the input string, until eventually taking all of it.
-#   # The longest string that passed the <isFunction> test becomes the candidate
-#   # (if the test passed at all)
-#   # TODO: optimisation possible = s'arrêter dès que isFunction devient faux (aucune chance que ça redevienne vrai)
-#   lastValid = 0
-#   for n in range(1,len(inputStr)+1) :
-#     (head,_) = split(inputStr,n)
-#     if (head in infixOps) :
-#       lastValid = n
-  
-#   return split(inputStr,lastValid)
-
-
-
-
-    
-
-
 def isAlpha(inputStr) :
   """
   Description:
@@ -964,9 +936,11 @@ def isDigit(inputStr) :
 def showInStr(inputStr, loc) :
   """
   Description:
-  Prints <inputStr> with a "^" char right below at a location
+  Prints <inputStr> with a "^" char right below the a location
   defined by <loc>.
   It helps to point out a specific char in the string.
+
+  <loc> shall point using a 0-indexing convention.
   """
   
   print(inputStr)  
@@ -1049,3 +1023,8 @@ if (__name__ == '__main__') :
   assert(qParser.consumeFunc("q(2.4, 0.1)") == ("", "q(2.4, 0.1)"))
   assert(qParser.consumeFunc("Q(2.4, 0.1)") == ("Q", "2.4, 0.1)"))
   print("- Passed: <consumeFunc>")
+
+
+
+
+  
